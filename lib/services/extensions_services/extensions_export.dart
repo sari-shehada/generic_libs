0d7978a922794d9f7c614abf9cacf0a5f911d0a9
Ismail Tayeb <tayebismail099@gmail.com>
2023-01-31 21:08:46 +0300

    Added new extenstion methods and isolates services

---
+++ lib/services/extensions_services/extensions_export.dart
@@ -0,0 +1,8 @@
+library extensions;
+
+import 'package:flutter/material.dart';
+
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+part 'numbers_extensions.dart';
+part 'color_service.dart';
