532b43f7749b0647df94c0b2a5d045360ad2ddd8
Ismail Tayeb <tayebismail099@gmail.com>
2023-02-01 22:42:16 +0300

    New Structure

---
+++ lib/alterable/design_constants_configurative.dart
@@ -0,0 +1,10 @@
+part of 'global_configurative.dart';
+
+abstract class DesignConstantsConfigurative {
+  //TODO: Set Default Shadows and BorderRadiuses
+  static const defaultRadiusButton = 12;
+  static const defaultRadiusButtonChild = 12;
+  static const defaultRadiusContainer = 12;
+  static const defaultRadiusCard = 12;
+  //TODO: Add Shadow
+}
--- lib/services/extensions_services/color_service.dart
+++ lib/services/extensions_services/color_service.dart
@@ -30,4 +30,6 @@ extension ColorsExtensions on Color {
     double grayScaleValue = (0.299 * red) + (0.587 * green) + (0.114 * blue);
     return grayScaleValue > 128;
   }
+
+  bool isColorDark() => !isColorLight();
 }
